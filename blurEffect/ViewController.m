60da280a98efae3bb7515068dbb694eb7d1b4f48
zhaochunyang0726 <zhaochunyang0726@163.com>
2015-08-05 14:49:56 +0800

    update

--- blurEffect/ViewController.m
+++ blurEffect/ViewController.m
@@ -9,6 +9,7 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UIView *redView;
 
 @end
 
@@ -17,6 +18,15 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    /**
+     *  ios8 以后才能使用的高斯模糊效果
+     */
+    UIBlurEffect *blur = [UIBlurEffect effectWithStyle:UIBlurEffectStyleLight];
+    UIVisualEffectView *effectView = [[UIVisualEffectView alloc] initWithEffect:blur];
+    effectView.frame = CGRectMake(0, 200, [UIScreen mainScreen].bounds.size.width, self.redView.bounds.size.height);
+    [self.view addSubview:effectView];
+
 }
 
 - (void)didReceiveMemoryWarning {
